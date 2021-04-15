27696aa9daeb712658c620e2365fafc9c7b11c70
timonchiks <tim.1010@yandex.ru>
2021-04-15 12:15:21 +0300

    homework

---
+++ homework/BigInteger/build/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/C++_projects/git2/HSE-Course/module-1/homework/BigInteger/build/googletest-src/googletest
+# Build directory: /mnt/d/C++_projects/git2/HSE-Course/module-1/homework/BigInteger/build/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ homework/Geometry/build/hierarchy/CMakeFiles/hierarchy.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/hierarchy/circle.cpp" "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/build/hierarchy/CMakeFiles/hierarchy.dir/circle.cpp.o"
+  "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/hierarchy/ellipse.cpp" "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/build/hierarchy/CMakeFiles/hierarchy.dir/ellipse.cpp.o"
+  "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/hierarchy/line.cpp" "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/build/hierarchy/CMakeFiles/hierarchy.dir/line.cpp.o"
+  "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/hierarchy/point.cpp" "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/build/hierarchy/CMakeFiles/hierarchy.dir/point.cpp.o"
+  "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/hierarchy/polygon.cpp" "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/build/hierarchy/CMakeFiles/hierarchy.dir/polygon.cpp.o"
+  "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/hierarchy/rectangle.cpp" "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/build/hierarchy/CMakeFiles/hierarchy.dir/rectangle.cpp.o"
+  "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/hierarchy/shape.cpp" "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/build/hierarchy/CMakeFiles/hierarchy.dir/shape.cpp.o"
+  "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/hierarchy/square.cpp" "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/build/hierarchy/CMakeFiles/hierarchy.dir/square.cpp.o"
+  "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/hierarchy/triangle.cpp" "/mnt/d/C++_projects/git2/HSE-Course/module-1/homework/Geometry/build/hierarchy/CMakeFiles/hierarchy.dir/triangle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../hierarchy"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
